e3c7bebf3bb063524351d570607d686dd879882b
Rebeca Pacheco <rbkgarner@gmail.com>
2021-06-10 13:39:35 -0300

    create credentials by email and password

--- src/components/CustomButtonAuth/CustomButtonAuth.tsx
+++ src/components/CustomButtonAuth/CustomButtonAuth.tsx
@@ -15,7 +15,7 @@ export const CustomButtonAuth = ({ providerId, keyProvider, children }: CustomBu
     switch (keyProvider) {
       case 'GitHub': {
         return (
-          <button className='btn btn-outline' onClick={() => signIn(providerId)}>
+          <button className='btn btn-primary' onClick={() => signIn(providerId)}>
             <AiFillGithub className='mr-2' size={20} />
             {children}
           </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Page() {
   const [session] = useSession()
 
   useEffect(() => {
-    console.log({ session })
+    console.log(session?.user)
   }, [])
 
   const callApi = async () => {
