342fc732559fee437fb19ec9fc4a44fe5e728a7a
Julio Makdisse Saito <saitodisse@gmail.com>
2021-06-10 12:30:30 -0300

    README

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useRef, useState } from 'react'
+import React, { ReactNode, useRef } from 'react'
 import Link from 'next/link'
 import { LinkProps } from '../../model/site/LinksList'
 import { ThemeList } from '../../model/site/ThemeList'
