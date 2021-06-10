712c8fc07410bcb6ed894a8293efcf15109a56b2
bluuesz <wosleyv@gmail.com>
2021-06-10 10:59:37 -0300

    add middleware admin

---
+++ src/utils/middleware/isAdmin.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/client'
+
+export interface NextApiRequestWithUser extends NextApiRequest {
+  user: {
+    name: string
+    role?: unknown
+    image: string
+    email: string
+  }
+}
+
+
+// HOC
+export const isAdmin = (handler: (req: NextApiRequestWithUser, res: NextApiResponse) => void) => {
+  return async (req: NextApiRequestWithUser, res: NextApiResponse): Promise<void> => {
+    const session = await getSession({ req })
+
+    if(!session?.user) {
+      return res.status(401).json({message: "you are not authenticated"})
+    }
+
+    if(session.user.role === "Admin") {
+      req.user = session.user
+      return handler(req, res)
+    } else {
+      return res.status(401).json({ message: "unauthorized" })
+    }
+  }
+}
