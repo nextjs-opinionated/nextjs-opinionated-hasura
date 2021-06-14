89e9f93bf0477f8a8e293512f25455acd1664433
Rebeca Pacheco <rbkgarner@gmail.com>
2021-06-14 10:48:43 -0300

    renaming variables

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,39 +1,40 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import React from 'react'
 
-const NEXT_PUBLIC_GA_ID = process.env.NEXT_PUBLIC_GA_ID
+const googleAnalytics = process.env.NEXT_PUBLIC_GA_ID
 
 export default class MyDocument extends Document {
   render() {
-    const NEXT_PUBLIC_SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME
-    const NEXT_PUBLIC_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL
-    const NEXT_PUBLIC_SITE_IMAGE = process.env.NEXT_PUBLIC_SITE_IMAGE
-    const NEXT_PUBLIC_SITE_DESCRIPTION = process.env.NEXT_PUBLIC_SITE_DESCRIPTION
-    const KEYWORDS = process.env.KEYWORDS
+    const pageTitle = process.env.NEXT_PUBLIC_SITE_NAME
+    const pageUrl = process.env.NEXT_PUBLIC_SITE_URL
+    const imageUrl = process.env.NEXT_PUBLIC_SITE_IMAGE
+    const description = process.env.NEXT_PUBLIC_SITE_DESCRIPTION
+    const keywords = process.env.KEYWORDS
+
     return (
       <Html lang='pt-br'>
         <Head>
           <link rel='icon' href='/favicon.ico' />
-          <meta name='description' content={NEXT_PUBLIC_SITE_DESCRIPTION} />
-          <meta name='keywords' content={KEYWORDS} />
+          <meta name='description' content={description} />
+          <meta name='keywords' content={keywords} />
 
           {/* Open Graph */}
-          <meta name='og:title' content={NEXT_PUBLIC_SITE_NAME} key='ogtitle' />
+          <meta name='og:title' content={pageTitle} key='ogtitle' />
           <meta name='og:type' content='website' />
-          <meta name='og:url' content={NEXT_PUBLIC_SITE_URL} />
-          <meta name='og:description' content={NEXT_PUBLIC_SITE_DESCRIPTION} />
-          <meta name='og:image' content={NEXT_PUBLIC_SITE_IMAGE} />
+          <meta name='og:url' content={pageUrl} />
+          <meta name='og:description' content={description} />
+          <meta name='og:image' content={imageUrl} />
 
           {/* Twitter */}
           <meta name='twitter:card' content='summary' key='twcard' />
           <meta name='twitter:creator' content='twitterHandle' key='twhandle' />
 
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {NEXT_PUBLIC_GA_ID?.length > 0 && (
+          {googleAnalytics?.length > 0 && (
             <>
               <script
                 async
-                src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GA_ID}`}
+                src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalytics}`}
               />
               <script
                 dangerouslySetInnerHTML={{
@@ -41,7 +42,7 @@ export default class MyDocument extends Document {
                       window.dataLayer = window.dataLayer || [];
                       function gtag(){dataLayer.push(arguments);}
                       gtag('js', new Date());
-                      gtag('config', '${NEXT_PUBLIC_GA_ID}', {
+                      gtag('config', '${googleAnalytics}', {
                         page_path: window.location.pathname,
                       });
                   `,
