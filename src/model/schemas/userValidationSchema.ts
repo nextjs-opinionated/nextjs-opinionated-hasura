c3d6d2ed49319b8146bfe6f9f461d940c4625e63
Wosley Vacemberg <wosleyv@gmail.com>
2021-06-16 14:59:00 -0300

    change schema folders

---
+++ src/model/schemas/userValidationSchema.ts
@@ -0,0 +1,22 @@
+// import _ from 'lodash'
+import * as z from 'zod'
+import { Roles_Enum } from '../../graphql/generated'
+import _ from "lodash"
+
+export const userValidationSchema = z.object({
+  id: z.any().refine(
+    (value) => {
+      // optional if is on insert mode
+      if (!value) {
+        return true
+      }
+      return _.isInteger(value)
+    },
+    {
+      message: 'id must be a valid integer',
+    }
+  ),
+  name: z.string().nonempty(),
+  email: z.string().email(),
+  role: z.nativeEnum(Roles_Enum)
+})
