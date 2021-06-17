dbd78016b6e30ab2309f65bf5987fe5e25987818
thailajf <thailajf@hotmail.com>
2021-06-17 11:47:21 -0300

    [image-field_component] add image stories

---
+++ src/components/forms/FormImage/FormImage.stories.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { FormImage, FormImageProps } from './FormImage'
+
+export default {
+  title: 'Component/Forms/FormImage',
+  component: FormImage,
+  argTypes: {
+    register: {
+      table: {
+        disable: true,
+      },
+    },
+    validationErrors: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta
+
+const Template: Story<FormImageProps> = (args) => <FormImage {...args} />
+
+export const Image_OK = Template.bind({})
+Image_OK.args = {
+  name: 'image_name',
+  register: () => {
+    /* noop */
+  },
+  validationErrors: {},
+}
+
+export const Image_DefaultValue = Template.bind({})
+Image_DefaultValue.args = {
+  title: 'Title',
+  name: 'image_name',
+  defaultValue:'http://daisyui.com/tailwind-css-component-profile-1@94w.png',
+  register: () => {
+    /* noop */
+  },
+
+  validationErrors: {},
+}
+
+export const Image_Label = Template.bind({})
+Image_Label.args = {
+  label: 'Label:',
+  name: 'image_name',
+  register: () => {
+    /* noop */
+  },
+  validationErrors: {},
+}
+
+export const Image_Error = Template.bind({})
+Image_Error.args = {
+  name: 'text_with_error',
+  register: () => {
+    /* noop */
+  },
+  validationErrors: {
+    text_with_error: {
+      message: 'Lorem ipsum dolor sit amet, consectetur adipiscing. Vestibulum rutrum metus at',
+    },
+  },
+}
+
+export const Image_CustomClassName = Template.bind({})
+Image_CustomClassName.args = {
+  label: 'Label:',
+  name: 'image_name',
+  register: () => {
+    /* noop */
+  },
+  validationErrors: {},
+  className: 'text-lg',
+}
