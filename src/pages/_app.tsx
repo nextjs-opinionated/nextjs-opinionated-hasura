59cc512862a954b74420b6f84d399216ede2570e
Rebeca Pacheco <rbkgarner@gmail.com>
2021-06-23 13:57:30 -0300

    init sentry setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '../styles/tailwind.css'
-import { ThemeProvider } from 'next-themes'
 import { StrictMode } from 'react'
+import { ThemeProvider } from 'next-themes'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
+
+Sentry.init({
+  dsn: 'https://afda10ad7e6e47ef99c2ed248abef638@o878762.ingest.sentry.io/5830876',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
 
 function MyApp({ Component, pageProps }) {
   return (
