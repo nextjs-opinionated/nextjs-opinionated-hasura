009043b58e841d1dafba89c4f5c89066c651e3cf
Wosley Vacemberg <wosleyv@gmail.com>
2021-06-25 14:59:29 -0300

    with next-connect

---
+++ src/pages/api/next-connect/hello2.ts
@@ -0,0 +1,33 @@
+import { FormExampleValidationSchema } from '../../../model/schemas/FormExampleValidationSchema'
+import handler from '../../../utils/next-connect'
+
+export default handler.post((req, res) => {
+  try {
+    try {
+      // server validation (VALIDATIONS)
+      FormExampleValidationSchema.parse(req.body)
+    } catch (error) {
+      console.error({ msg: 'validation:', error })
+      if (error?.errors) {
+        res.status(500).json(error.errors)
+        return
+      }
+      res.status(500).json(error)
+      return
+    }
+
+    const dataObj = {
+      email: req.body.email,
+      image: req.body.email,
+    }
+
+    res.json({ dataObj, server_time: new Date().toLocaleTimeString() })
+  } catch (e) {
+    console.error('Request error', e)
+    res.statusCode = 500
+    res.json({
+      error: e.message,
+    })
+  }
+})
+  
