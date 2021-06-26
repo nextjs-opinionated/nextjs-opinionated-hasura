35aac1cf1e26783003a47d3f640c39224150e49c
Wosley Vacemberg <wosleyv@gmail.com>
2021-06-26 19:50:59 -0300

    update error

--- src/pages/api/sentry-error.ts
+++ src/pages/api/sentry-error.ts
@@ -2,14 +2,14 @@ import { withSentry } from '@sentry/nextjs'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { logMiddleware } from '../../utils/middleware/log'
 
-const throwKnownError = () => Promise.reject(new Error('API Test 1'))
-throwKnownError() // only test sentry
+const throwKnownError = () => Promise.reject(new Error('Sentry Error'))
 
 
 export default withSentry(logMiddleware((_: NextApiRequest, res: NextApiResponse) => {
   const response = {message: `This must have generated an error in sentry. Check the console`}
   
-  res.status(200).json(response)
+  throwKnownError() // only test sentry
+  return res.status(200).json(response)
 }))
 
 
