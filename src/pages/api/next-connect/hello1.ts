913020dfc69ffddff29f31684f7d644f803e0ba6
Julio Saito <saitodisse@gmail.com>
2021-06-29 13:19:16 -0300

    [logger] withSentry on typedFetch apis

--- src/pages/api/next-connect/hello1.ts
+++
@@ -1,7 +0,0 @@
-import handler from '../../../utils/next-connect'
-
-export default handler.get((_, res) => {
-  const response = {message: `Hello from server! It's ${new Date().toLocaleTimeString()}`}
-  
-  res.status(200).json(response)
-})
--- src/utils/logger/logger.ts
+++ src/utils/logger/logger.ts
@@ -3,8 +3,5 @@ import pino from 'pino'
 // create pino loggger
 export const logger = pino({
   level: 'debug',
-  prettyPrint: { colorize: true, timestampKey: "time" },
+  prettyPrint: { colorize: true, timestampKey: 'time' },
 })
-
-export const log = (msg: unknown) => logger.info({ data: msg })
-
