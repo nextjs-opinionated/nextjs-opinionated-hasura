3a87be350efb4ca06f21eb5c7d8b2255701fffac
Julio Saito <saitodisse@gmail.com>
2021-07-03 14:37:39 -0300

    change LinksList order

--- src/model/site/LinksList.ts
+++ src/model/site/LinksList.ts
@@ -1,9 +1,9 @@
 export type LinksListIds =
   | 'home'
-  | 'github'
   | 'styleGuide'
-  | 'form-example'
   | 'typed-fetch-examples'
+  | 'form-example'
+  | 'github'
 
 export type LinkProps = {
   id: LinksListIds
@@ -25,15 +25,15 @@ export const LinksList: {
     name: 'Style Guide',
     internalURL: '/style-guide',
   },
-  'form-example': {
-    id: 'form-example',
-    name: 'React Form Example',
-    internalURL: '/form-example',
-  },
   'typed-fetch-examples': {
     id: 'typed-fetch-examples',
     name: 'Typed-Fetch',
     internalURL: '/typed-fetch-examples',
+  },
+  'form-example': {
+    id: 'form-example',
+    name: 'React Form Example',
+    internalURL: '/form-example',
   },
   github: {
     id: 'github',
