294a9a1b71a73f6b51783ddd9df903329e736f96
thailajf <thailajf@hotmail.com>
2021-07-06 09:11:36 -0300

    [table] add image option

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -2,6 +2,7 @@ import classnames from 'classnames'
 import Link from 'next/link'
 import React from 'react'
 import { MdDelete } from 'react-icons/md'
+import { FaUserAlt } from 'react-icons/fa'
 
 export interface TableProps {
   data: any[]
@@ -40,8 +41,40 @@ export const Table: React.FC<TableProps> = ({
                     {/* the first field is mandatory for the detail's link and should have 'id' field */}
                     {index === 0 && linkPage && value.id ? (
                       <Link href={`${linkPage}/${value.id}`}>
-                        <a className='pl-0 underline btn btn-link btn-xs'>{value[fieldName]} </a>
+                        <a className='pl-0 underline btn btn-link btn-xs'>
+                          {fieldName === 'image' ? (
+                            <div className='flex items-center space-x-3'>
+                              {value[fieldName] ? (
+                                <div className='avatar'>
+                                  <div className='w-12 h-12 mask mask-squircle'>
+                                    <img src={value[fieldName]} />
+                                  </div>
+                                </div>
+                              ) : (
+                                <div className='flex flex-row items-center justify-center w-12 h-12 mask mask-squircle bg-base-300'>
+                                  <FaUserAlt size={25} />
+                                </div>
+                              )}
+                            </div>
+                          ) : (
+                            value[fieldName]
+                          )}
+                        </a>
                       </Link>
+                    ) : fieldName === 'image' ? (
+                      <div className='flex items-center space-x-3'>
+                        {value[fieldName] ? (
+                          <div className='avatar'>
+                            <div className='w-12 h-12 mask mask-squircle'>
+                              <img src={value[fieldName]} alt={fieldName} />
+                            </div>
+                          </div>
+                        ) : (
+                          <div className='flex flex-row items-center justify-center w-12 h-12 mask mask-squircle bg-base-300'>
+                            <FaUserAlt size={25} />
+                          </div>
+                        )}
+                      </div>
                     ) : (
                       value[fieldName]
                     )}
