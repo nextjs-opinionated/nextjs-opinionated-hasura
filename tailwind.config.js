59d09f28055ded9a30cdaa26c1b4ef8e0f8f99e4
Wosley Vacemberg <wosleyv@gmail.com>
2021-07-06 15:40:21 -0300

    add @tailwindcss/typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('daisyui'), require('@tailwindcss/forms')],
+  plugins: [require('daisyui'), require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
