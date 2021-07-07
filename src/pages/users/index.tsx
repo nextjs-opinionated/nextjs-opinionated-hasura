a98add503a7c54e9d8734f1c28ef1ec248885f3a
thailajf <thailajf@hotmail.com>
2021-07-07 12:04:28 -0300

    [Apply_Table] add table component to users page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,13 @@
 import { Layout } from '../../components/Layout/Layout'
 import { Roles_Enum } from '../../graphql/generated'
-import dayjs from 'dayjs'
 import Link from 'next/link'
 import { getInitialNameAvatar } from '../../utils/getInitialNameAvatar'
 import { LinksList } from '../../model/site/LinksList'
 import { useQuery } from 'react-query'
 import { Users_api_get, users_api_get_Config } from '../../model/api-models/users/Users_api_get'
 import typedFetch from '../../utils/typedFetch/typedFetch'
+import React, { useState } from 'react'
+import { Table } from '../../components/Table/Table'
 
 const handleBadgeRole = (role: Roles_Enum) => {
   switch (role) {
@@ -23,9 +24,13 @@ const handleBadgeRole = (role: Roles_Enum) => {
 }
 
 export default function Page() {
+  const [current_page, current_pageSet] = useState(1)
+  const ITEMS_PER_PAGE = 6
+
   const { data, isLoading, error } = useQuery('users_api_get', async () => {
     const resultObj = await typedFetch<Users_api_get['input'], Users_api_get['output']>({
       ...users_api_get_Config,
+      data: { limit: ITEMS_PER_PAGE.toString(), current_page: current_page.toString() },
     })
     return resultObj.data
   })
@@ -48,50 +53,32 @@ export default function Page() {
       }
       menuItems={Object.values(LinksList)}
     >
-      <div className='overflow-x-auto'>
-        <table className='table w-full'>
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Email</th>
-              <th>Função</th>
-            </tr>
-          </thead>
-          {data &&
-            data?.users?.length > 0 &&
-            data?.users.map((user) => (
-              <tbody key={user.id}>
-                <tr>
-                  <td>
-                    <div className='flex items-center space-x-3'>
-                      <div className='avatar'>
-                        <div className='w-12 h-12 mask mask-squircle'>
-                          <img
-                            src={user.image ? user.image : getInitialNameAvatar(user.name)}
-                            alt='Avatar Tailwind CSS Component'
-                          />
-                        </div>
-                      </div>
-                      <div>
-                        <div className='font-bold'>{user.name}</div>
-                        <div className='text-sm opacity-50'>
-                          {dayjs(user.created_at).format('DD-MM-YYYY')}
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-                  <td>{user.email}</td>
-                  <td>{handleBadgeRole(user.role as Roles_Enum)}</td>
-                  <th>
-                    <Link href={`/users/${user.id}`}>
-                      <button className='btn btn-ghost btn-xs'>details</button>
-                    </Link>
-                  </th>
-                </tr>
-              </tbody>
-            ))}
-        </table>
-      </div>
+      <Table
+        pageSize={ITEMS_PER_PAGE}
+        totalItems={data?.users_aggregate?.aggregate?.count}
+        currentPage={current_page}
+        onPageSet={current_pageSet}
+        className='table-zebra'
+        data={data?.users || []}
+        fields={{
+          Título: (item) => (
+            <Link href={`/users/${item.id}`}>
+              <a title={`Editar os dados de ${item.name}`}>
+                <div className='avatar'>
+                  <div className='w-12 h-12 mask mask-squircle'>
+                    <img
+                      src={item.image ? item.image : getInitialNameAvatar(item.name)}
+                      alt='Avatar Tailwind CSS Component'
+                    />
+                  </div>
+                </div>
+              </a>
+            </Link>
+          ),
+          'E-mail/Nome': (item) => item.email || item.name,
+          'Função ': (item) => handleBadgeRole(item.role as Roles_Enum),
+        }}
+      />
     </Layout>
   )
 }
