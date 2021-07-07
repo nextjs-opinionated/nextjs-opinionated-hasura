2a5fbead2291e4ce79f3218dfd8b0191a74c1f93
thailajf <thailajf@hotmail.com>
2021-07-07 07:23:20 -0300

    [table_component] remove unused and desnecessary code from table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,8 +1,6 @@
 import classnames from 'classnames'
-import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import { MdDelete } from 'react-icons/md'
-import { FaUserAlt } from 'react-icons/fa'
 import { Pagination } from '../Pagination/Pagination'
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
@@ -12,8 +10,6 @@ export interface TableProps {
   data: any[]
   fields: { [key: string]: (item: any) => React.ReactNode }
   className?: string
-  fieldNames: string[]
-  urlPrefix?: string
   pageSize?: number
   currentPage?: number
   totalItems?: number
@@ -27,9 +23,7 @@ export interface TableProps {
 export const Table: React.FC<TableProps> = ({
   fields,
   data,
-  fieldNames,
   className = '',
-  urlPrefix,
   pageSize = 5,
   currentPage = 1,
   totalItems,
@@ -40,15 +34,6 @@ export const Table: React.FC<TableProps> = ({
   deleteConfirmationNoLabel = 'No',
 }) => {
   const [totalPage, totalPageSet] = useState<number>(0)
-
-  useEffect(() => {
-    if (fields) {
-      _.map(fields, (value, key) => {
-        console.log('--  key: ', key)
-        console.log('--  value: ', value)
-      })
-    }
-  }, [fields])
 
   useEffect(() => {
     if (totalItems) {
@@ -81,50 +66,6 @@ export const Table: React.FC<TableProps> = ({
                       {value(item)}
                     </td>
                   ))}
-
-                  {/* fieldNames.map((fieldName, index) => (
-                    <td key={`${index}-td`}>
-                      {index === 0 && urlPrefix && value.id ? (
-                        <Link href={`${urlPrefix}/${value.id}`}>
-                          <a className='pl-0 underline btn btn-link btn-xs'>
-                            {fieldName === 'image' ? (
-                              <div className='flex items-center space-x-3'>
-                                {value[fieldName] ? (
-                                  <div className='avatar'>
-                                    <div className='w-12 h-12 mask mask-squircle'>
-                                      <img src={value[fieldName]} />
-                                    </div>
-                                  </div>
-                                ) : (
-                                  <div className='flex flex-row items-center justify-center w-12 h-12 mask mask-squircle bg-base-300'>
-                                    <FaUserAlt size={25} />
-                                  </div>
-                                )}
-                              </div>
-                            ) : (
-                              value[fieldName]
-                            )}
-                          </a>
-                        </Link>
-                      ) : fieldName === 'image' ? (
-                        <div className='flex items-center space-x-3'>
-                          {value[fieldName] ? (
-                            <div className='avatar'>
-                              <div className='w-12 h-12 mask mask-squircle'>
-                                <img src={value[fieldName]} alt={fieldName} />
-                              </div>
-                            </div>
-                          ) : (
-                            <div className='flex flex-row items-center justify-center w-12 h-12 mask mask-squircle bg-base-300'>
-                              <FaUserAlt size={25} />
-                            </div>
-                          )}
-                        </div>
-                      ) : (
-                        value[fieldName]
-                      )}
-                    </td>
-                      ))} */}
 
                   {onDelete && (
                     <td>
