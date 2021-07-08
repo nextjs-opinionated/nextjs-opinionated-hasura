5f8510ca70fbf8ccafacda11ba4060f23ad7813c
thailajf <thailajf@hotmail.com>
2021-07-08 15:28:32 -0300

    [list_item] fix list_items_api to use pagination

--- src/pages/api/list-items/list_items_api_get.ts
+++ src/pages/api/list-items/list_items_api_get.ts
@@ -7,6 +7,7 @@ import { HttpStatusCode } from '../../../utils/typedFetch/HttpStatusCode'
 import { withSentry } from '@sentry/nextjs'
 import { logMiddleware } from '../../../utils/middleware/logMiddleware'
 import { dataList } from '../../../model/datas/list-items'
+import _ from 'lodash'
 
 export default withSentry(
   logMiddleware(async function API(req: NextApiRequest, res: NextApiResponse) {
@@ -21,15 +22,25 @@ export default withSentry(
       res.status(HttpStatusCode.METHOD_NOT_ALLOWED_405).end(`Method ${req.method} Not Allowed`)
     }
 
-    //const inputData = req.query as List_Item_api_get['input']
+    const inputData = req.query as List_Item_api_get['input']
 
-    // const limit = _.toNumber(inputData.limit)
-    // const current_page = _.toNumber(inputData.current_page)
-    // const offset = current_page * limit - limit
+    const limit = _.toNumber(inputData.limit)
+    const current_page = _.toNumber(inputData.current_page)
+    const offset = current_page * limit - limit
+
+    const listItems: List_Item_api_get['output']['list_items'] = []
+    for (let x = offset; x < offset + limit; x++) {
+      if (dataList.data.list_items[x]) {
+        listItems.push(dataList.data.list_items[x])
+      }
+    }
+
     // process
-    const data: List_Item_api_get['output'] = dataList.data
 
-    // await new GqlSdkHelper().getSdk().list_items({ limit, offset })
+    const data: List_Item_api_get['output'] = {
+      list_items: listItems,
+      list_items_aggregate: dataList.data.list_items_aggregate,
+    }
 
     // output data
     res.status(HttpStatusCode.OK_200).json(data)
